9b62507828af2e56cb9c1d4121721d47b3d31d1c
nikitawanjale <wanjale@unlv.nevada.edu>
2015-12-01 01:48:17 -0800

    Update main.c

--- main.c
+++ main.c
@@ -6,7 +6,6 @@
 #include "driverlib/debug.h"
 #include "driverlib/sysctl.h"
 #include "driverlib/adc.h"
-#include "driverlib/gpio.h"
 
 #ifdef DEBUG
 void__error__(char *pcFilename, uint32_t ui32Line)
@@ -16,49 +15,45 @@ void__error__(char *pcFilename, uint32_t ui32Line)
 
 int main(void)
 {
+	//create an array that will be used for storing the data read from the ADC FIFO
 	uint32_t ui32ADC0Value[4];
 
+	//The first variable is for storing the average of the temperature.
+	//The remaining variables are used to store the temperature values for Celsius and Fahrenheit.
 	volatile uint32_t ui32TempAvg;
 	volatile uint32_t ui32TempValueC;
 	volatile uint32_t ui32TempValueF;
 
+	//Set up the system clock
 	SysCtlClockSet(SYSCTL_SYSDIV_5|SYSCTL_USE_PLL|SYSCTL_OSC_MAIN|SYSCTL_XTAL_16MHZ);
 
+	//Enable the ADC peripheral
 	SysCtlPeripheralEnable(SYSCTL_PERIPH_ADC0);
-	SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF);
 	//ADCHardwareOversampleConfigure(ADC0_BASE, 64);
 
-	GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE, GPIO_PIN_1);
+	//Capture the value of channel zero using SS1
+	ADCSequenceConfigure(ADC0_BASE, 1, ADC_TRIGGER_PROCESSOR, 0);
 
-	//Use sequencer 2
-	ADCSequenceConfigure(ADC0_BASE, 2, ADC_TRIGGER_PROCESSOR, 0);
+	//Configure steps 0 - 2 on sequencer 1 to sample the temperature sensor
+	ADCSequenceStepConfigure(ADC0_BASE, 1, 0, ADC_CTL_TS);
+	ADCSequenceStepConfigure(ADC0_BASE, 1, 1, ADC_CTL_TS);
+	ADCSequenceStepConfigure(ADC0_BASE, 1, 2, ADC_CTL_TS);
+	ADCSequenceStepConfigure(ADC0_BASE,1,3,ADC_CTL_TS|ADC_CTL_IE|ADC_CTL_END);
 
-	ADCSequenceStepConfigure(ADC0_BASE, 2, 0, ADC_CTL_TS);
-	ADCSequenceStepConfigure(ADC0_BASE, 2, 1, ADC_CTL_TS);
-	ADCSequenceStepConfigure(ADC0_BASE, 2, 2, ADC_CTL_TS);
-	ADCSequenceStepConfigure(ADC0_BASE,2,3,ADC_CTL_TS|ADC_CTL_IE|ADC_CTL_END);
-	ADCSequenceEnable(ADC0_BASE, 2);
+	ADCSequenceEnable(ADC0_BASE, 1);
+
 	while(1)
 	{
-		ADCIntClear(ADC0_BASE, 2);
-		ADCProcessorTrigger(ADC0_BASE, 2);
-		while(!ADCIntStatus(ADC0_BASE, 2, false))
+		ADCIntClear(ADC0_BASE, 1);
+		ADCProcessorTrigger(ADC0_BASE, 1);
+		while(!ADCIntStatus(ADC0_BASE, 1, false))
 		{
 		}
-		ADCSequenceDataGet(ADC0_BASE, 2, ui32ADC0Value);
+		ADCSequenceDataGet(ADC0_BASE, 1, ui32ADC0Value);
 		ui32TempAvg = (ui32ADC0Value[0] + ui32ADC0Value[1] + ui32ADC0Value[2] + ui32ADC0Value[3] + 2)/4;
 		ui32TempValueC = (1475 - ((2475 * ui32TempAvg)) / 4096)/10;
 		ui32TempValueF = ((ui32TempValueC *	9) + 160) /	5;
 
-		//Turn the LED on if the temperature is above 80F
-		if(ui32TempValueF > 80)
-		{
-			GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1, 0xFF);
-		}
-		else
-		{
-			GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1, 0x00);
-		}
 	}
 }
 
